38f62cda3bdc04b74c1bc09a6d726a599dc35fc9
Vishal Pawar <vishalpawar425@gmail.com>
2023-09-23 17:29:40 +0530

    feat: Added the timer functionality

--- A01 - Pomodoro/start/src/App.js
+++ A01 - Pomodoro/start/src/App.js
@@ -1,19 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
+function addPad(timer) {
+  return timer.toString().padStart(2 , 0);
+}
+
 export default function App() {
+  const [title, setTitle] = useState("Let the countdown begin!!!")
+  const [timer, setTimer] = useState(5);
+
+
+  function startTimer() {
+    setInterval(() => {
+      setTimer(timer => {
+        if(timer>=1) return timer - 1;
+
+        return 0;
+      });
+    }, 1000);
+  }
+
+  const minutes = addPad(Math.floor(timer / 60));
+  const seconds = addPad(timer - minutes * 60);
+
   return (
     <div className="app">
-      <h2>Pomodoro!</h2>
+      <h2>{title}</h2>
 
       <div className="timer">
-        <span>00</span>
+        <span>{minutes}</span>
         <span>:</span>
-        <span>00</span>
+        <span>{seconds}</span>
       </div>
 
       <div className="buttons">
-        <button>Start</button>
+        <button onClick={startTimer}>Start</button>
         <button>Stop</button>
         <button>Reset</button>
       </div>
